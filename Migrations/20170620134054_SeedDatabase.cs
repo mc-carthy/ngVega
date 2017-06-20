baf87265d5583de7a0b940dddd0d5581ee4b6cba
Mick McCarthy <mccarthy.mick0012@gmail.com>
2017-06-20 14:49:45 +0100

    Seed database

---
+++ Migrations/20170620134054_SeedDatabase.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Vega.Migrations
+{
+    public partial class SeedDatabase : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("INSERT INTO Makes (Name) VALUES ('Audi')");
+            migrationBuilder.Sql("INSERT INTO Makes (Name) VALUES ('BMW')");
+            migrationBuilder.Sql("INSERT INTO Makes (Name) VALUES ('Mercedes')");
+
+            // The below code is inefficient, we should query the database for each MakeID once and store it in a variable.
+            // Because we have such a small number of recurds, it's fine.
+
+            migrationBuilder.Sql("INSERT INTO Models (Name, MakeID) VALUES ('A3', (SELECT ID FROM Makes WHERE Name = 'Audi'))");
+            migrationBuilder.Sql("INSERT INTO Models (Name, MakeID) VALUES ('A5', (SELECT ID FROM Makes WHERE Name = 'Audi'))");
+            migrationBuilder.Sql("INSERT INTO Models (Name, MakeID) VALUES ('A6', (SELECT ID FROM Makes WHERE Name = 'Audi'))");
+            migrationBuilder.Sql("INSERT INTO Models (Name, MakeID) VALUES ('1-Series', (SELECT ID FROM Makes WHERE Name = 'BMW'))");
+            migrationBuilder.Sql("INSERT INTO Models (Name, MakeID) VALUES ('3-Series', (SELECT ID FROM Makes WHERE Name = 'BMW'))");
+            migrationBuilder.Sql("INSERT INTO Models (Name, MakeID) VALUES ('5-Series', (SELECT ID FROM Makes WHERE Name = 'BMW'))");
+            migrationBuilder.Sql("INSERT INTO Models (Name, MakeID) VALUES ('A-Class', (SELECT ID FROM Makes WHERE Name = 'Mercedes'))");
+            migrationBuilder.Sql("INSERT INTO Models (Name, MakeID) VALUES ('C-Class', (SELECT ID FROM Makes WHERE Name = 'Mercedes'))");
+            migrationBuilder.Sql("INSERT INTO Models (Name, MakeID) VALUES ('E-Class', (SELECT ID FROM Makes WHERE Name = 'Mercedes'))");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("DELETE FROM Makes WHERE Name IN ('Audi', 'BMW', 'Mercedes')");
+        }
+    }
+}
