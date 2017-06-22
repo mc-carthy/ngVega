ea2c9e1abbd098cf3be4d247c23d63643abc121b
Mick McCarthy <mccarthy.mick0012@gmail.com>
2017-06-22 13:49:51 +0100

    Map VehicleResource API resource to Vehicle domain class

--- Controllers/VehiclesController.cs
+++ Controllers/VehiclesController.cs
@@ -1,4 +1,6 @@
+using AutoMapper;
 using Microsoft.AspNetCore.Mvc;
+using ngVega.Controllers.Resources;
 using ngVega.Models;
 
 namespace ngVega.Controllers
@@ -6,11 +8,17 @@ namespace ngVega.Controllers
     [Route("/api/vehicles")]
     public class VehiclesController : Controller
     {
+        private readonly IMapper mapper;
+        public VehiclesController(IMapper mapper)
+        {
+            this.mapper = mapper;
+        }
         // This use of a Domain class as a parameter is temporary until we implement the API Resource
         // The [FromBody] attribute tells the method that the data for the properties are set in the body of the request
         [HttpPost()]
-        public IActionResult CreateVehicle([FromBody] Vehicle vehicle)
+        public IActionResult CreateVehicle([FromBody] VehicleResource vehicleResource)
         {
+            var vehicle = mapper.Map<VehicleResource, Vehicle>(vehicleResource);
             return Ok(vehicle);
         }
     }
--- Mapping/MappingProfile.cs
+++ Mapping/MappingProfile.cs
@@ -1,3 +1,4 @@
+using System.Linq;
 using AutoMapper;
 using ngVega.Controllers.Resources;
 using ngVega.Models;
@@ -9,9 +10,18 @@ namespace ngVega.Mapping
         public MappingProfile()
         {
             // Ensure the property names match in order for AutoMapper to work correctly
+
+            // Domain to API Resource
             CreateMap<Make, MakeResource>();
             CreateMap<Model, ModelResource>();
             CreateMap<Feature, FeatureResource>();
+
+            // API Resource to Domain
+            CreateMap<VehicleResource, Vehicle>()
+                .ForMember(v => v.ContactName, opt => opt.MapFrom(vr => vr.Contact.Name))
+                .ForMember(v => v.ContactPhone, opt => opt.MapFrom(vr => vr.Contact.Email))
+                .ForMember(v => v.ContactPhone, opt => opt.MapFrom(vr => vr.Contact.Phone))
+                .ForMember(v => v.Features, opt => opt.MapFrom(vr => vr.Features.Select(id => new VehicleFeature{ FeatureId = id})));
         }
     }
 }
