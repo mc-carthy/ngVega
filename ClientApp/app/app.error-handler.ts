69ebd8ee3b3de269fc457d14721fb657a8eb817b
Mick McCarthy <mccarthy.mick0012@gmail.com>
2017-06-23 20:03:38 +0100

    Use Zones to ensure toasty notification displays correctly

--- ClientApp/app/app.error-handler.ts
+++ ClientApp/app/app.error-handler.ts
@@ -1,19 +1,24 @@
-import { ErrorHandler, Inject } from "@angular/core";
+import { ErrorHandler, Inject, NgZone } from "@angular/core";
 import { ToastyService } from 'ng2-toasty';
 
 export class AppErrorHandler implements ErrorHandler
 {
-    constructor(@Inject(ToastyService) private toastyService: ToastyService) 
+    constructor(
+        private ngZone: NgZone,
+        @Inject(ToastyService) private toastyService: ToastyService
+        ) 
     {
     }
 
-    handleError(error: any): void {       
-        this.toastyService.error({
-            title: 'Error',
-            msg: 'An unexpected error occurred',
-            theme: 'bootstrap',
-            showClose: true,
-            timeout: 5000
-        });
+    handleError(error: any): void { 
+        this.ngZone.run(() => {
+            this.toastyService.error({
+                title: 'Error',
+                msg: 'An unexpected error occurred',
+                theme: 'bootstrap',
+                showClose: true,
+                timeout: 5000
+            });
+        })
     }
 }
